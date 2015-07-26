44de45c23be55f9d694ced4a2f0885b1cb9515f9
Acey <acey1@users.noreply.github.com>
2015-07-26 11:38:30 +0530

    Update gridcoin.rb

--- gridcoin.rb
+++ gridcoin.rb
@@ -1,7 +1,7 @@
 class Gridcoin < Formula
   desc "GridCoin OS X client (GUI and CLI)"
   homepage "http://gridcoin.us"
-  "https://github.com/gridcoin/Gridcoin-Research.git"
+  url "https://github.com/gridcoin/Gridcoin-Research.git"
   
   option "with-cli", "Also compile the command line client"
   option "without-gui", "Do not compile the graphical client"
